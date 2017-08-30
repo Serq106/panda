417941888a1b5ee6b0150a0898527ca12c97d7e9
Sten <serq106@mail.ru>
2017-08-30 10:33:48 +0300

    fix autocomplete on main view

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,80 @@
 function AjaxFormRequest(url_video, uid, type) {
-	jQuery.ajax({
-		url:     "bd.php", 
-		type:     "POST", 
-		data: {urls:url_video, id:uid, types:type},
-		success: function(response) { 
-			alert (type);
-		}
-	});
+    jQuery.ajax({
+        url: "bd.php",
+        type: "POST",
+        data: {urls: url_video, id: uid, types: type},
+        success: function (response) {
+            alert(type);
+        }
+    });
 }
 
 function AjaxFormPost(id_user, url_photo, name, id_post, id_photo, texts, type, uid) {
-	jQuery.ajax({
-		url:     "bd.php", 
-		type:     "POST", 
-		data: {id_user:id_user, url_photo:url_photo, name:name, id_post:id_post, id_photo:id_photo, texts:texts, types:type, id:uid},
-		success: function(response) { 
-			alert (type);
-		}
-	});
+    jQuery.ajax({
+        url: "bd.php",
+        type: "POST",
+        data: {
+            id_user: id_user,
+            url_photo: url_photo,
+            name: name,
+            id_post: id_post,
+            id_photo: id_photo,
+            texts: texts,
+            types: type,
+            id: uid
+        },
+        success: function (response) {
+            alert(type);
+        }
+    });
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-var inProgress = false;
-var startFrom = 10;
-	
-    $(window).scroll(function() {
-        if($(window).scrollTop() + $(window).height() >= $(document).height() - 300 && !inProgress) {
-		
-        $.ajax({            
-            url: 'obrabotchik.php',
-            method: 'POST',
-            data: {"startFrom" : startFrom},
-            beforeSend: function() {
-				inProgress = true;
-			}
-            /* что нужно сделать по факту выполнения запроса */            
-            }).done(function(data){
-            
-            data = jQuery.parseJSON(data);
-            
-            if (data.length > 0) {            
-				$.each(data, function(index, data){
-            
-            /* Отбираем по идентификатору блок со статьями и дозаполняем его новыми данными */    
-			if(data[0,0] == "video")
-				$("#content").append("<div class='video'><div id='video12'><iframe src='" + data[1] + "' allowfullscreen='' frameborder='0'></iframe></div><input type='button' value='Добавить!' onclick=\"AjaxFormRequest('"+data[1]+"', '"+data[2]+"', '"+data[0]+"');\" ></div>");
-			else if(data[0,0] == "post")
-			{
-				if(data[2] > 0)
-					$("#content").append("<div class='post_grup'><div class='ava'><div class='top_logo'><a href='https://vk.com/id" + data[2] + "'> <img src='" + data[5] +"' class='photo'> <span class='text'>" + data[6] +" <span> </a> <a href='https://vk.com/'> <img src='https://img3.goodfon.ru/wallpaper/middle/f/2c/vk-vkontakte-logo-vk.jpg' class='logo'> </a> </div> <a href='https://vk.com/search?w=wall" +data[2]+"_"+data[1]+"'> <div class='phost_photo'> <img src='"+data[3]+"'> </div> <p>"+data[4]+"</p></a><input type='button' value='Добавить!' onclick=\"AjaxFormPost('"+data[2]+"', '"+data[5]+"', '"+data[6]+"', '"+data[1]+"', '"+data[3]+"','"+data[4]+"', '"+data[0]+"','"+data[7]+"');\" ></div></div>");
-				else if(data[2] < 0)
-					$("#content").append("<div class='post_grup'><div class='ava'><div class='top_logo'><a href='https://vk.com/public" + data[5] + "'> <img src='" + data[6] +"' class='photo'> <span class='text'>" + data[7] +" "+  data[7] + "<span> </a> <a href='https://vk.com/'> <img src='https://img3.goodfon.ru/wallpaper/middle/f/2c/vk-vkontakte-logo-vk.jpg' class='logo'> </a> </div> <a href='https://vk.com/search?w=wall" +data[2]+"_"+data[1]+"'> <div class='phost_photo'> <img src='"+data[3]+"'> </div> <p>"+data[4]+"</p></a><input type='button' value='Добавить!' onclick=\"AjaxFormPost('"+data[2]+"', '"+data[5]+"', '"+data[6]+"', '"+data[1]+"', '"+data[3]+"','"+data[4]+"', '"+data[0]+"','"+data[7]+"');\" ></div></div>");
+    var inProgress = false;
+    var startFrom = 10;
 
-			} else if(data[0,0] == "human"){
-				$("#content").append("<div class='user'><a href='https://vk.com/id"+data[1]+"'><div class='user_photo' ><img src='"+data[2]+"' style='display: inline-block;'><span class='unfo_user' style='position: absolute;'>"+data[3]+" "+data[4]+"<span></div></a></div>");
-			} 
-			else if(data[0,0] == "videoY"){
-				$("#content").append("<div class='video'><div id='video12'><iframe src='" + data[1] + "' allowfullscreen frameborder='0'></iframe></div><input type='button' value='Добавить!' onclick=\"AjaxFormRequest('"+data[1]+"', '"+data[2]+"', '"+data[0]+"');\" ></div>");
-			}
-			
+    $(window).scroll(function () {
+        if ($(window).scrollTop() + $(window).height() >= $(document).height() - 300 && !inProgress) {
+
+            $.ajax({
+                url: 'obrabotchik.php',
+                method: 'POST',
+                data: {"startFrom": startFrom},
+                beforeSend: function () {
+                    inProgress = true;
+                }
+                /* что нужно сделать по факту выполнения запроса */
+            }).done(function (data) {
+
+                data = jQuery.parseJSON(data);
+
+                if (data.length > 0) {
+                    $.each(data, function (index, data) {
+
+                        /* Отбираем по идентификатору блок со статьями и дозаполняем его новыми данными */
+                        if (data[0, 0] == "video")
+                            $("#content").append("<div class='video'><div id='video12'><iframe src='" + data[1] + "' allowfullscreen='' frameborder='0'></iframe></div><input type='button' value='Добавить!' onclick=\"AjaxFormRequest('" + data[1] + "', '" + data[2] + "', '" + data[0] + "');\" ></div>");
+                        else if (data[0, 0] == "post") {
+                            if (data[2] > 0)
+                                $("#content").append("<div class='post_grup'><div class='ava'><div class='top_logo'><a href='https://vk.com/id" + data[2] + "'> <img src='" + data[5] + "' class='photo'> <span class='text'>" + data[6] + " <span> </a> <a href='https://vk.com/'> <img src='https://img3.goodfon.ru/wallpaper/middle/f/2c/vk-vkontakte-logo-vk.jpg' class='logo'> </a> </div> <a href='https://vk.com/search?w=wall" + data[2] + "_" + data[1] + "'> <div class='phost_photo'> <img src='" + data[3] + "'> </div> <p>" + data[4] + "</p></a><input type='button' value='Добавить!' onclick=\"AjaxFormPost('" + data[2] + "', '" + data[5] + "', '" + data[6] + "', '" + data[1] + "', '" + data[3] + "','" + data[4] + "', '" + data[0] + "','" + data[7] + "');\" ></div></div>");
+                            else if (data[2] < 0)
+                                $("#content").append("<div class='post_grup'><div class='ava'><div class='top_logo'><a href='https://vk.com/public" + data[5] + "'> <img src='" + data[6] + "' class='photo'> <span class='text'>" + data[7] + " " + data[7] + "<span> </a> <a href='https://vk.com/'> <img src='https://img3.goodfon.ru/wallpaper/middle/f/2c/vk-vkontakte-logo-vk.jpg' class='logo'> </a> </div> <a href='https://vk.com/search?w=wall" + data[2] + "_" + data[1] + "'> <div class='phost_photo'> <img src='" + data[3] + "'> </div> <p>" + data[4] + "</p></a><input type='button' value='Добавить!' onclick=\"AjaxFormPost('" + data[2] + "', '" + data[5] + "', '" + data[6] + "', '" + data[1] + "', '" + data[3] + "','" + data[4] + "', '" + data[0] + "','" + data[7] + "');\" ></div></div>");
+
+                        } else if (data[0, 0] == "human") {
+                            $("#content").append("<div class='user'><a href='https://vk.com/id" + data[1] + "'><div class='user_photo' ><img src='" + data[2] + "' style='display: inline-block;'><span class='unfo_user' style='position: absolute;'>" + data[3] + " " + data[4] + "<span></div></a></div>");
+                        }
+                        else if (data[0, 0] == "videoY") {
+                            $("#content").append("<div class='video'><div id='video12'><iframe src='" + data[1] + "' allowfullscreen frameborder='0'></iframe></div><input type='button' value='Добавить!' onclick=\"AjaxFormRequest('" + data[1] + "', '" + data[2] + "', '" + data[0] + "');\" ></div>");
+                        }
+
+                    });
+
+                    inProgress = false;
+                    startFrom += 10
+
+                }
             });
-			
-            inProgress = false;
-            startFrom += 10
-			
-            }});   
         }
     });
 });
--- obrabotchik.php
+++ obrabotchik.php
@@ -1,94 +1,89 @@
 <?php
-
-	session_start();	
+	
+	session_start();
 	$text = $_SESSION['s_text'];
 	$type = $_SESSION['s_type'];
 	$access_token = $_SESSION['s_access_token'];
 	$access_token_youtube = $_SESSION['s_access_token_youtube'];
 	$startFrom = $_POST['startFrom'];
-	$uid = $_COOKIE["uid"];	
+	$uid = $_COOKIE["uid"];
 	include_once("method.php");
 	
-	if(($text !='' && $type == "video")){
-		$result =  video_search($text, 10, $startFrom, $access_token);
-			
+	if (($text != '' && $type == "video")) {
+		$result = video_search($text, 10, $startFrom, $access_token);
+		
 		$articles = array();
-		for($i=0; $i< 10; $i++)
-		{
-			$url_video = $result -> response -> items[$i] -> player;
-			$articles[] =  array("video" ,$url_video, $uid);
+		for ($i = 0; $i < 10; $i++) {
+			$url_video = $result->response->items[ $i ]->player;
+			$articles[] = array("video", $url_video, $uid);
 		}
 		echo json_encode($articles);
 		
-	} else if(($text !='' && $type == "posts")){
-			$text=str_replace(" ","%20",$text);
-			$f=str_replace("#","%23",$text);
-			$u_info = newsfeed_search($f, 10, $startFrom , 5.12, $access_token);
-
-			
+	} else if (($text != '' && $type == "posts")) {
+		$text = str_replace(" ", "%20", $text);
+		$f = str_replace("#", "%23", $text);
+		$u_info = newsfeed_search($f, 10, $startFrom, 5.12, $access_token);
+		
+		
 		$articles = array();
-		for($i=0; $i< 10; $i++)
-		{
-			$id_post = $u_info -> response -> items[$i] -> id;
-			$id_user = $u_info -> response -> items[$i] -> owner_id;
-			$id_photo = $u_info -> response -> items[$i] -> attachments[0] -> photo -> photo_604;	
-			$text = $u_info -> response -> items[$i] -> text;
-			if ($user_id > 0){
+		for ($i = 0; $i < 10; $i++) {
+			$id_post = $u_info->response->items[ $i ]->id;
+			$id_user = $u_info->response->items[ $i ]->owner_id;
+			$id_photo = $u_info->response->items[ $i ]->attachments[0]->photo->photo_604;
+			$text = $u_info->response->items[ $i ]->text;
+			if ($user_id > 0) {
 				$result = users_get($id_user, "photo_50", 5.52, $access_token);
-				$url_photo =$result ->response[0] -> photo_50;
-				$first_name =$result ->response[0] -> first_name;
-				$last_name =$result ->response[0] -> last_name;
-				$name = $first_name." ".$last_name;
-				$articles[] = array("post" ,$id_post, $id_user, $id_photo, $text, $url_photo, $name, $uid) ;
-
-			} else {	
-				$id_group =  abs($id_user);							
+				$url_photo = $result->response[0]->photo_50;
+				$first_name = $result->response[0]->first_name;
+				$last_name = $result->response[0]->last_name;
+				$name = $first_name . " " . $last_name;
+				$articles[] = array("post", $id_post, $id_user, $id_photo, $text, $url_photo, $name, $uid);
+				
+			} else {
+				$id_group = abs($id_user);
 				$result = groups_getById($id_user, 5.63, $access_token);
-				$url_photo = $result ->response[0] -> photo_50;
-				$name_group = $result ->response[0] -> name;
-				$articles[] = array("post" ,$id_post, $id_user, $id_photo, $text, $id_group, $url_photo, $name_group, $uid) ;
+				$url_photo = $result->response[0]->photo_50;
+				$name_group = $result->response[0]->name;
+				$articles[] = array("post", $id_post, $id_user, $id_photo, $text, $id_group, $url_photo, $name_group, $uid);
 			}
 			
 		}
 		echo json_encode($articles);
-	} else if((/*$text !='' && */$type == "human")){
+	} else if ((/*$text !='' && */
+		$type == "human")) {
 		$last_name = $_SESSION['s_last_name'];
 		$first_name = $_SESSION['s_first_name'];
-		if ($last_name != null && $first_name != null){
-			$q = $last_name.'%20'.$first_name;
-		} else if ($last_name != null && $first_name == null)
-		{
+		if ($last_name != null && $first_name != null) {
+			$q = $last_name . '%20' . $first_name;
+		} else if ($last_name != null && $first_name == null) {
 			$q = $last_name;
-		}else if ($last_name == null && $first_name != null)
-		{
+		} else if ($last_name == null && $first_name != null) {
 			$q = $first_name;
 		}
 		$result = users_search($q, 392, 5.63, $access_token, $startFrom);
-
-			
+		
+		
 		$articles = array();
-		for($i=0; $i< 10; $i++)
-		{	
-				$id_user = $result -> response -> items[$i] -> id;						
-				$url_photo = $result -> response -> items[$i] -> photo_200;
-				$first_name = $result -> response -> items[$i] -> first_name;
-				$last_name = $result -> response -> items[$i] -> last_name;
-				$articles[] = array("human" ,$id_user, $url_photo, $first_name, $last_name) ;
-
+		for ($i = 0; $i < 10; $i++) {
+			$id_user = $result->response->items[ $i ]->id;
+			$url_photo = $result->response->items[ $i ]->photo_200;
+			$first_name = $result->response->items[ $i ]->first_name;
+			$last_name = $result->response->items[ $i ]->last_name;
+			$articles[] = array("human", $id_user, $url_photo, $first_name, $last_name);
+			
 		}
 		echo json_encode($articles);
-	} else if(($text !='' && $type == "videoY")){
+	} else if (($text != '' && $type == "videoY")) {
 		$nextPageToken = $_SESSION['nextPageToken'];
 		$result = video_search_youtube("snippet", 10, $text, $nextPageToken, $access_token_youtube);
-		$_SESSION['nextPageToken'] = $result -> nextPageToken;
-		$e = $result -> nextPageToken;		
+		$_SESSION['nextPageToken'] = $result->nextPageToken;
+		$e = $result->nextPageToken;
 		$articles = array();
-		for($i=0; $i< 10; $i++)
-		{
-			$url_video = $result ->items[$i] -> id -> videoId;
-			$url_video = 'https://www.youtube.com/embed/'.$url_video;
-			$articles[] =  array("videoY", $url_video, $uid);
+		for ($i = 0; $i < 10; $i++) {
+			$url_video = $result->items[ $i ]->id->videoId;
+			$url_video = 'https://www.youtube.com/embed/' . $url_video;
+			$articles[] = array("videoY", $url_video, $uid);
 		}
-		echo json_encode($articles);	
+		echo json_encode($articles);
 	}
 ?>
